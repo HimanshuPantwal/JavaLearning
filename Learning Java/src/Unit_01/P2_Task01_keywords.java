b03c82052b3631049cff7bdf7b131c02e25234a2
HimanshuPantwal <himanshupantwal5@gmail.com>
2022-03-31 13:25:01 +0530

    Commit

---
+++ Learning Java/src/Unit_01/P2_Task01_keywords.java
@@ -0,0 +1,25 @@
+package Unit_01;
+
+public class P2_Task01_keywords {
+	class Student
+	{
+		int height;
+	}
+    public static void main(String[] args)
+    {
+    	//Syntax error on token "float", invalid VariableDeclaratorId
+    	int float=2.3;
+    	float int=2;
+    	int char ="Himanshu";
+    	 
+        System.out.println();
+        //Syntax error on token "int", delete this token
+        Student int=new Student;
+    }
+    //syntax error on token "float", Identifier expected
+    void float()
+    {
+    	System.out.println("helloworld");
+    }
+}
+
