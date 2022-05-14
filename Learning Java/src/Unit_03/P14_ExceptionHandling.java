9cd9d9fe5c41fb66a9d01573d68d9f9a6ef1e541
HimanshuPantwal <himanshupantwal5@gmail.com>
2022-05-14 23:42:28 +0530

    commit

--- Learning Java/src/Unit_03/P14_ExceptionHandling.java
+++ Learning Java/src/Unit_03/P14_ExceptionHandling.java
@@ -1,83 +1,125 @@
 package Unit_03;
-import java.util.*;
-//exception handling
-//-->use of try and catch block
-//-->why try and catch block;
-//-->difference btw error exception
-//-->use of throw keyword
-//-->use of final
-/*file handling 
- 1.read input from a file instead of console
- 2.making a log of execution pattern
- 3.multi threading
-*/
-//Java Program to Demonstrate Exception is Thrown
-//How the runTime System Searches Call-Stack
-//to Find Appropriate Exception Handler
-
-//Class
-//ExceptionThrown
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class P14_ExceptionHandling {
- public static void main(String arg[])
- {
-	 exception ex=new exception();
-	 ex.BasicException();
-	 ex.Division();
-	 ex.outOfBound();
-	// ex.stackoverflow(); *it cannot be resolved using exception handling as well
-	 ex.mismatch();
- }
-}
- class exception{
-	 Scanner p=new Scanner(System.in);
-	 void BasicException(){
-	 int a=0;
-	 int b=100/a;
-	 }
-	 
-	 void Division() {
-		 int a=100;
-		 int b=0;
+
+	public static void main(String[] args){
+		BasicException2();
+		BasicException3();
+		BasicException4();
+		BasicException5(1);
+		BasicException6();
+		checkedAndUncheckedException();
+		BasicException7();
+		useOfThrows_And_Finally();
+		checkName("Himanshu");
+	}
+	static void BasicException2() {
+		int a=100,ans=0;
+		try {
+			ans=a/0;
+		}catch(ArithmeticException e){
+			System.out.println("divide by zero");
+		}		
+	}
+	static void BasicException3() {
+		int a=100,ans=0;
+		try {
+			ans=a/0;
+		}catch(ArithmeticException e){
+			System.out.println("divide by zero");
+		}finally {
+			System.out.println("Final block");
+		}
+	}
+	static void BasicException4() {
+		int a;
+		Scanner sc=new Scanner(System.in);
+		try {
+			a=sc.nextInt();			
+		}catch(InputMismatchException e) {
+			System.out.println("Wrong Input");
+		}	
+	}
+	static void BasicException5(int a) {
+		try {
+			while(a>0) {
+				a++;BasicException5(a);
+			}
+		}catch(Exception e){
+			System.out.println("error");
+		}
 		
-		 try {
-			 int res=a/b; 
-		 }
-		 catch(Exception e)
-		 {
-			 System.out.println("exception occured");
-		 }
-	 }
-	 void outOfBound() {
-		 int[] arr=new int[4];
-		 arr[0]=4;
-		 arr[1]=5;
-		 arr[2]=8;
-		 arr[3]=9;
-		 try {
-			 int x=arr[3];
-			 System.out.println("value is:"+x);
-		 }
-		 catch(Exception ex) {
-			 System.out.println("out of bound");
-		 }
-	 }
-	 void stackoverflow() {
-		 try {
-			 stackoverflow();
-		 }
-		 catch(Exception x)
-		 {
-			 System.out.println("stack overflow");
-		 }
-	 }
-	 void mismatch() {
-		 try {
-			 System.out.println("enter the number");
-			 int x=p.nextInt();
-		 }
-		 catch(Exception e) {
-			 System.out.println("wrong input");
-		 }
-	 }
+	}
+	static void BasicException6() {
+		int arr[]=new int[3];
+		arr[0]=1;arr[1]=2;arr[2]=3;
+		try {
+			System.out.println(arr[10]);
+		}catch(IndexOutOfBoundsException e){
+			System.out.println("Out of bounds");
+		}
+	}
+	static void BasicException7() {
+		try {
+			int a=(Integer) null;
+		}catch(NullPointerException e){
+			System.out.println("Null");
+		}
+	}
+	static void useOfThrow(int age) throws Exception, ArithmeticException, IOException {
+
+		if (age < 18) {
+			throw new Exception("Under 18");
+		} else {
+			System.out.println("You are allowed to enter");
+		}
+
+	}
+
+	static void useOfThrows_And_Finally() {
+
+		try {
+			useOfThrow(10);
+		} catch (ArithmeticException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		for(int i=0;i<3;i++)
+			System.out.println("continue");
+
+	}
+	static void checkedAndUncheckedException() {
+		int x = 0;
+		int y = 10;
+		int z = y / x;
+
+		System.out.println(z);
+		try {
+			useOfThrow(19);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	} 
+	static void checkName(String s){
+		try {
+			if(s.charAt(0)>95) {
+				throw new IncorrectName();
+			}
+		}catch(IncorrectName e){
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
+@SuppressWarnings("serial")
+class IncorrectName extends Exception{
+	public IncorrectName() {
+		super("First letter should be capital");
+	}
 }
